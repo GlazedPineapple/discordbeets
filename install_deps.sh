4ad02eec692c1832eb41dda2e447d1c1f9f50008
glazedpineapple <eokovacs1@gmail.com>
2020-05-21 22:34:59 -0400

    fixed ssh agian

--- install_deps.sh
+++ install_deps.sh
@@ -4,9 +4,8 @@ set -e
 # Apt packages
 sudo apt update
 sudo apt upgrade -y
-sudo apt install gcc libclang make automake autoconf libtool pkg-config audiopus_sys ffmpeg -y
+sudo apt install gcc clang make automake autoconf libtool pkg-config ffmpeg -y
 
 # Youtube dl
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
-
