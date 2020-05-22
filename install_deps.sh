98c0ad6bef8f294c85e35303d58e6cdef9fce3ab
glazedpineapple <eokovacs1@gmail.com>
2020-05-21 22:34:19 -0400

    fix: ssh script

--- install_deps.sh
+++ install_deps.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
+set -e
 
 # Apt packages
 sudo apt update
 sudo apt upgrade -y
-Sudo apt install gcc libclang make automake autoconf libtool pkg-config audiopus_sys ffmpeg -y
-
+sudo apt install gcc libclang make automake autoconf libtool pkg-config audiopus_sys ffmpeg -y
 
 # Youtube dl
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
